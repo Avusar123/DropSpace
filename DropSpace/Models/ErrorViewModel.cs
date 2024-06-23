a6c4fdd66e53abfceaa8caee90a3194f145edc41
Avusar <Avusar@DESKTOP-NPB65FI>
2024-06-23 15:58:57 +0300

    Добавьте файлы проекта.

---
+++ DropSpace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DropSpace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
