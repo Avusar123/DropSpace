45b53998b40003b56444fd5fb7255b70b4fccc4e
Артём Варлапов <varlapov2005@mail.ru>
2024-10-22 23:40:33 +0300

    rpcPath fix

---
+++ DropSpace.FrontEnd/HttpHandlers/RpcHttpHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using DropSpace.FrontEnd.Utils;
+using DropSpace.FrontEnd.Utils.ErrorHandlers;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Refit;
+
+namespace DropSpace.FrontEnd.HttpHandlers
+{
+    public class RpcHttpHandler(
+        AuthManager tokenProvider, 
+        ErrorHandlerFactory errorHandlerFactory,
+        IConfiguration  configuration) : TokenHttpHandler(tokenProvider, errorHandlerFactory)
+    {
+        protected override Task ConfigureRequest(HttpRequestMessage request)
+        {
+            if (request.RequestUri == null)
+            {
+                throw new NullReferenceException("Запрос пуст!");
+            }
+
+            var uriBuilder = new UriBuilder();
+
+            uriBuilder.Path = configuration["rpcPrefix"] + request.RequestUri.AbsolutePath;
+
+            request.RequestUri = uriBuilder.Uri;
+
+            return base.ConfigureRequest(request);
+        }
+    }
+}
